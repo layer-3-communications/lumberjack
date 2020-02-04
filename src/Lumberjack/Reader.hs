8eae592208666bc913b95a5d9cdcf2ee71de2764
Andrew Martin <andrew.thaddeus@gmail.com>
2020-02-04 15:51:02 -0500

    Eliminate unaligned memory accesses

--- src/Lumberjack/Reader.hs
+++ src/Lumberjack/Reader.hs
@@ -2,6 +2,7 @@
 {-# language BlockArguments #-}
 {-# language DataKinds #-}
 {-# language LambdaCase #-}
+{-# language ScopedTypeVariables #-}
 {-# language TypeApplications #-}
 
 module Lumberjack.Reader
@@ -20,10 +21,9 @@ import Socket.Stream.IPv4 (Connection)
 import Socket.Stream.IPv4 (Interruptibility(Uninterruptible))
 import Socket.Stream.IPv4 (SendException(..),ReceiveException(..))
 import Data.Chunks (Chunks)
-import Data.Bytes (Bytes)
+import Data.Bytes.Types (Bytes(Bytes))
 import Data.Primitive (ByteArray)
-import Socket.Stream.Uninterruptible.MutableBytes (send)
-import Socket.Stream.Uninterruptible.Bytes (receiveExactly)
+import Socket.Stream.Uninterruptible.MutableBytes (send,receiveExactly)
 
 import qualified Data.Builder.ST as Builder
 import qualified Data.Bytes as Bytes
@@ -33,6 +33,7 @@ import qualified Data.Primitive.ByteArray.BigEndian as BE
 import qualified Data.Bytes.Parser.Latin as Latin
 import qualified Data.Bytes.Parser.BigEndian as BE
 import qualified Data.Bytes.Parser as Parser
+import qualified Socket.Stream.Uninterruptible.Bytes as StreamBytes
 import qualified Zlib
 
 data Frame
@@ -104,65 +105,67 @@ decompress !raw = do
 -- | Receive and frame and decode its messages. This acknowledges every
 -- frame after receiving it. 
 read :: Connection -> IO (Either Exception Frame)
-read conn = receiveExactly conn (1 + 1 + 4) >>= \case
-  Left err -> case err of
-    ReceiveShutdown -> pure (Left ClosedConnection)
-    ReceiveReset -> pure (Left ResetConnection)
-    ReceiveHostUnreachable -> pure (Left Unreachable)
-  Right barr -> do
-    let version = PM.indexByteArray barr 0 :: Word8
-        frameType = PM.indexByteArray barr 1 :: Word8
-        u32 = BE.indexUnalignedByteArray barr 2 :: Word32
-    case version of
-      0x32 -> case frameType of
-        -- Ignore the maximum unacknowledged frame count since we always
-        -- send acks every time we receive anything.
-        0x57 -> read conn  
-        -- We do not support data frames yet.
-        0x44 -> pure (Left ReceivedData)
-        -- Compressed payloads are very similar to JSON payloads.
-        -- The user is responsible for performing decompression.
-        0x43 -> do
-          let szW = u32
-          if szW > 16777216
-            then pure (Left LargeFrame)
-            else do
-              let sz = fromIntegral @Word32 @Int szW
-              receiveExactly conn sz >>= \case
-                Left err -> case err of
-                  ReceiveShutdown -> pure (Left ClosedConnectionPoorly)
-                  ReceiveReset -> pure (Left ResetConnection)
-                  ReceiveHostUnreachable -> pure (Left Unreachable)
-                Right payload -> case decompress (Bytes.fromByteArray payload) of
-                  Right chunks -> ack (lastSequenceNumber chunks) conn >>= \case
+read conn = do
+  recvBuf <- PM.newByteArray 8
+  receiveExactly conn (MutableBytes recvBuf 2 6) >>= \case
+    Left err -> case err of
+      ReceiveShutdown -> pure (Left ClosedConnection)
+      ReceiveReset -> pure (Left ResetConnection)
+      ReceiveHostUnreachable -> pure (Left Unreachable)
+    Right (_ :: ()) -> do
+      version :: Word8 <- PM.readByteArray recvBuf 2
+      frameType :: Word8 <- PM.readByteArray recvBuf 3
+      u32 :: Word32 <- BE.readByteArray recvBuf 1
+      case version of
+        0x32 -> case frameType of
+          -- Ignore the maximum unacknowledged frame count since we always
+          -- send acks every time we receive anything.
+          0x57 -> read conn  
+          -- We do not support data frames yet.
+          0x44 -> pure (Left ReceivedData)
+          -- Compressed payloads are very similar to JSON payloads.
+          -- The user is responsible for performing decompression.
+          0x43 -> do
+            let szW = u32
+            if szW > 16777216
+              then pure (Left LargeFrame)
+              else do
+                let sz = fromIntegral @Word32 @Int szW
+                StreamBytes.receiveExactly conn sz >>= \case
+                  Left err -> case err of
+                    ReceiveShutdown -> pure (Left ClosedConnectionPoorly)
+                    ReceiveReset -> pure (Left ResetConnection)
+                    ReceiveHostUnreachable -> pure (Left Unreachable)
+                  Right payload -> case decompress (Bytes payload 0 sz) of
+                    Right chunks -> ack (lastSequenceNumber chunks) conn >>= \case
+                      Left err -> case err of
+                        SendShutdown -> pure (Left ClosedConnectionPoorly)
+                        SendReset -> pure (Left ResetConnection)
+                      Right _ -> pure (Right (Compressed payload chunks))
+                    Left err -> pure (Left err)
+          -- It is up to the user to decode the JSON payload.
+          0x4A -> do
+            let szW = u32
+            if szW > 16777216
+              then pure (Left LargeFrame)
+              else do
+                let sz = fromIntegral @Word32 @Int szW
+                    seqNo = u32
+                StreamBytes.receiveExactly conn sz >>= \case
+                  Left err -> case err of
+                    ReceiveShutdown -> pure (Left ClosedConnectionPoorly)
+                    ReceiveReset -> pure (Left ResetConnection)
+                    ReceiveHostUnreachable -> pure (Left Unreachable)
+                  Right payload -> ack seqNo conn >>= \case
                     Left err -> case err of
                       SendShutdown -> pure (Left ClosedConnectionPoorly)
                       SendReset -> pure (Left ResetConnection)
-                    Right _ -> pure (Right (Compressed payload chunks))
-                  Left err -> pure (Left err)
-        -- It is up to the user to decode the JSON payload.
-        0x4A -> do
-          let szW = u32
-          if szW > 16777216
-            then pure (Left LargeFrame)
-            else do
-              let sz = fromIntegral @Word32 @Int szW
-                  seqNo = u32
-              receiveExactly conn sz >>= \case
-                Left err -> case err of
-                  ReceiveShutdown -> pure (Left ClosedConnectionPoorly)
-                  ReceiveReset -> pure (Left ResetConnection)
-                  ReceiveHostUnreachable -> pure (Left Unreachable)
-                Right payload -> ack seqNo conn >>= \case
-                  Left err -> case err of
-                    SendShutdown -> pure (Left ClosedConnectionPoorly)
-                    SendReset -> pure (Left ResetConnection)
-                  Right _ -> do
-                    let !payload' = Bytes.fromByteArray payload
-                    pure (Right (Uncompressed (Json seqNo payload')))
-        _ -> pure (Left InvalidFrameType)
-      0x31 -> pure (Left VersionOne)
-      _ -> pure (Left InvalidVersion)
+                    Right _ -> do
+                      let !payload' = Bytes.fromByteArray payload
+                      pure (Right (Uncompressed (Json seqNo payload')))
+          _ -> pure (Left InvalidFrameType)
+        0x31 -> pure (Left VersionOne)
+        _ -> pure (Left InvalidVersion)
 
 ack :: Word32 -> Connection -> IO (Either (SendException 'Uninterruptible) ())
 ack !seqNo !conn = do
